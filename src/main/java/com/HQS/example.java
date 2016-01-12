1446ea9e46662c354c168a92bd522edac2ccf175
hxj8936270@vip.qq.com <12345678>
2016-01-12 22:18:56 +0800

    Initial commit

---
+++ src/main/java/com/HQS/example.java
@@ -0,0 +1,7 @@
+package com.HQS;
+
+/**
+ * Created by xj on 2016/1/12.
+ */
+public class example {
+}
---
+++ src/main/java/com/HQS/service/example.java
@@ -0,0 +1,7 @@
+package com.HQS.service;
+
+/**
+ * Created by xj on 2016/1/12.
+ */
+public class example {
+}
